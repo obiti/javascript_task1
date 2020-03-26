36c9d399a7bbf41b82565102d586c53d2916001c
obiti <62120211+obiti@users.noreply.github.com>
2020-03-26 23:44:45 +0100

    JAVASCRIPT task
    A loop of even numbers from 1- 200

---
+++ JOSEPH.js
@@ -0,0 +1,24 @@
+let name = "Joseph";
+
+ console.log("My name is: " + name);
+
+var course = ["HTML/CSS", "Javascript"];
+
+ console.log(course.length)
+
+ const data = [];
+
+ for (let i=0; i<=200; i++) {
+
+   if (i%2===0) {
+
+     data.push(i);
+
+   }
+
+ }
+
+ console.log(data);
+
+ 
+
